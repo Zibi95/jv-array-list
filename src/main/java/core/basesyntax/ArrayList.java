802c411cf7644a5db397b0dc2d067a47ba883da1
Zibi95 <sylwester.sob95@gmail.com>
2026-05-24 12:49:45 +0200

    fix suggestions

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 import java.util.NoSuchElementException;
-import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
@@ -82,7 +81,7 @@ public class ArrayList<T> implements List<T> {
 
     private int indexOf(T element) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(element, elements[i])) {
+            if ((elements[i] == null ? element == null : elements[i].equals(element))) {
                 return i;
             }
         }
