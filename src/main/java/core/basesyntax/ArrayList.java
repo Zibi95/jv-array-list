88bf484857878053ce6dda65c812b0c6178c33b1
Zibi95 <sylwester.sob95@gmail.com>
2026-05-24 12:46:19 +0200

    add ArrayList logic

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,104 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] elements = new Object[DEFAULT_CAPACITY];
+    private int size = 0;
+
     @Override
     public void add(T value) {
-
+        if (this.size == elements.length) {
+            this.increaseSize();
+        }
+        this.elements[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-
+        checkIndexBounds(index);
+        if (size == elements.length) {
+            increaseSize();
+        }
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndexBoundsExcludingLastElement(index);
+        return (T) elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndexBoundsExcludingLastElement(index);
+        elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndexBoundsExcludingLastElement(index);
+        T element = get(index);
+        size--;
+        System.arraycopy(elements, index + 1, elements, index, size - index);
+        return element;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        int index = indexOf(element);
+        if (index == -1) {
+            throw new NoSuchElementException("No such element");
+        }
+        return remove(index);
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
+    }
+
+    private void increaseSize() {
+        Object[] newElements = new Object[size + (size >> 1)];
+        System.arraycopy(elements, 0, newElements, 0, size);
+        elements = newElements;
+    }
+
+    private int indexOf(T element) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(element, elements[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void checkIndexBounds(int index) {
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index: " + index + ", but size: " + size);
+        }
+    }
+
+    private void checkIndexBoundsExcludingLastElement(int index) {
+        if (index == size) {
+            throw new ArrayListIndexOutOfBoundsException("Index: " + index + ", but size: " + size);
+        }
+        checkIndexBounds(index);
     }
 }
